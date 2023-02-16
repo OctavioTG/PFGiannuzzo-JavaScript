e85790288ac95bbb066466b6c766fdaeb576e794
Octavio Giannuzzo <octaviotgb@gmail.com>
2023-02-16 17:28:19 -0300

    entrega final

--- script.js
+++ script.js
@@ -1,75 +1,130 @@
-let productos = [
-    {id: 1, nombre: "KEN-L", categoria: "perro", precio: 7000, img:"./imgs/kenL.png"},
-    {id: 3, nombre: "EXCELENT", categoria: "perro", precio: 9500, img:"./imgs/excellent.png"},
-    {id: 4, nombre: "CAT-CHOW", categoria: "gato", precio: 8000, img:"./imgs/catChow.png"},
-    {id: 2, nombre: "GATI", categoria: "gato", precio: 6500, img:"./imgs/gati.png"},
-]
+async function peticion() {
+  const resp = await fetch("./productos.json")
+  const productosDB = await resp.json()
+  miPrograma(productosDB)
+}
+peticion()
 
-let productBox = document.querySelector("#productBox")
-let carritoBox = document.querySelector("#carritoBox")
+function miPrograma(productos) {
+  let carrito = localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : []
 
-let carrito = []
-if (localStorage.getItem("carrito")) {
-    let carritoEnJSON = localStorage.getItem("carrito")
-    carrito = JSON.parse(carritoEnJSON)}
+let productBox = document.querySelector("#productBox");
+let carritoBox = document.querySelector("#carritoBox");
 
-function renderProductos() {
-    productos.forEach(producto => {
-        let productCard = document.createElement("div")
-        productCard.classList.add("producto")
-        
-        productCard.innerHTML = `
+renderProductos(productos);
+
+function renderProductos(productos) {
+  productBox.innerHTML = "";
+  productos.forEach((producto) => {
+    let productCard = document.createElement("div");
+    productCard.classList.add("producto", "mt-3");
+
+    productCard.innerHTML = `
         <h3>${producto.nombre}</h3>
+        <p>${producto.edad}</p>
         <p>$${producto.precio}</p>
         <img class="img" src=${producto.img} />
-        <button class="button" id=${producto.id}>AGREGAR AL CARRITO</button>
-        `
-        productBox.append(productCard)
-        let boton = document.getElementById(producto.id)
-        boton.onclick = add
-    })
+        <button class="button d-block" id=${producto.id}>AGREGAR AL CARRITO</button>
+        `;
+    productBox.append(productCard);
+    let boton = document.getElementById(producto.id);
+    boton.onclick = add;
+  });
 }
-renderProductos()
 
 function add(e) {
-    let id = e.target.id
-    let productoSelec = productos.find(producto => producto.id == id)
-    if(carrito.includes(productoSelec)){
-        let productRepetido = carrito.indexOf(productoSelec)
-        carrito[productRepetido].unidades++
-    }else(
-        productoSelec.unidades = 1,
-        carrito.push(productoSelec)
-    )
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    renderCarrito()
+  let id = e.target.id;
+  let productoSelec = productos.find((producto) => producto.id == id);
+  if (carrito.includes(productoSelec)) {
+    let productRepetido = carrito.indexOf(productoSelec);
+    carrito[productRepetido].unidades++;
+  } else (productoSelec.unidades = 1), carrito.push(productoSelec);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  Toastify({
+    text: "Producto Agregado",
+
+    duration: 2000,
+  }).showToast();
+  renderCarrito();
 }
 
 function renderCarrito() {
-    let totalCarrito = carrito.reduce((acum, carrito) => acum + carrito.unidades * carrito.precio, 0)
-    carritoBox.innerHTML = `
-    <h2 class="m-3">Carrito:</h2>
+  let totalCarrito = carrito.reduce(
+    (acum, carrito) => acum + carrito.unidades * carrito.precio,
+    0
+  );
+  carritoBox.innerHTML = `
     <p>Su Total es = $${totalCarrito}</p>
-    `
-    carrito.forEach(producto => {
-        let carritoCard = document.createElement("div")
-        carritoCard.classList.add("m-3")
-        carritoCard.innerHTML +=`
+    `;
+  carrito.forEach((producto) => {
+    let carritoCard = document.createElement("div");
+    carritoCard.classList.add("m-3");
+    carritoCard.innerHTML += `
         <h5>${producto.nombre}</h5>
+        <p>${producto.edad}</p>
         <p>Unidades:${producto.unidades}</p>
-        `
-        carritoBox.appendChild(carritoCard)
-    })
-
+        `;
+    carritoBox.appendChild(carritoCard);
+  });
 }
+renderCarrito();
 
-let comprar = document.getElementById("comprar")
-comprar.addEventListener("click", finalizarCompra)
+let comprar = document.getElementById("comprar");
+comprar.addEventListener("click", finalizarCompra);
 
 function finalizarCompra() {
-    localStorage.removeItem("carrito")
-    carrito = []
-    renderCarrito()
+  localStorage.removeItem("carrito");
+  Swal.fire("Compra Exitosa", "Gracias por elegirnos", "success");
+  carrito = [];
+  renderCarrito();
 }
 
-console.log(carrito);
+let vaciar = document.getElementById("vaciar");
+vaciar.addEventListener("click", vaciarCarrito);
+
+function vaciarCarrito() {
+  localStorage.removeItem("carrito");
+  Toastify({
+    text: "Productos Eliminados",
+    duration: 2000,
+  }).showToast();
+  carrito = [];
+  renderCarrito();
+}
+
+// Filtro
+let buscador = document.querySelector("#buscador");
+let btnBuscador = document.querySelector("#btnBuscador");
+
+function filtrar() {
+  let filtrado = productos.filter(
+    (producto) =>
+      producto.nombre.toLowerCase().includes(buscador.value.toLowerCase()) ||
+      producto.categoria.toLowerCase().includes(buscador.value.toLowerCase())
+  );
+  renderProductos(filtrado);
+}
+
+btnBuscador.onclick = filtrar;
+
+let perro = document.getElementById("perro");
+perro.onclick = filtrarCategoria;
+let gato = document.getElementById("gato");
+gato.onclick = filtrarCategoria;
+let adulto = document.getElementById("adulto");
+adulto.onclick = filtrarEdad;
+let cachorro = document.getElementById("cachorro");
+cachorro.onclick = filtrarEdad;
+
+function filtrarCategoria(e) {
+  let btnCategoria = productos.filter(
+    ({ categoria }) => categoria === e.target.id
+  );
+  renderProductos(btnCategoria);
+}
+
+function filtrarEdad(e) {
+  let btnEdad = productos.filter(({ edad }) => edad === e.target.id);
+  renderProductos(btnEdad);
+}
+}
